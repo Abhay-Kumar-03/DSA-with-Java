48c5ad5774989d72ec6f21e4778de73518a4bcb7
Abhay-Kumar-03 <abhaychauhan8052@gmail.com>
2025-04-02 10:59:30 +0530

    commit

--- Arrays/g_DeleteElementAndCreatingNewArray.java
+++ Arrays/g_DeleteElementAndCreatingNewArray.java
@@ -12,6 +12,30 @@ public class g_DeleteElementAndCreatingNewArray {
             myArr[index] = input.nextInt();
             index++;
         }
+        System.out.print("Enter that element which you want to delete from this array: ");
+        int num = input.nextInt();
+        int occur = occur(myArr, num);
+        System.out.print(occur);
+        int[] newArray = new int[size - occur];
+        System.out.println(newArray);
 
     }
+
+    public static int occur(int[] myArr,  int num){
+        int count = 0;
+        int index = 0;
+        while(index < myArr.length){
+            if(myArr[index] == num){
+                count++;
+            }
+            index++;
+        }
+        return count;
+    }
+
+    public static int[] newArray(int[] myArr , int num){
+        int count = occur(myArr, num);
+
+    }
+
 }
